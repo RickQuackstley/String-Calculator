817ab193fdd6908f6e2083f37f9c0006a0312ef4
Rick Quackstley <103292732+RickQuackstley@users.noreply.github.com>
2022-06-12 17:12:11 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace CalculatorFixed
+{
+    class Program
+    {
+        static string equation = "";
+        //your equation goes above
+
+        static char[] operators = { '+', '-', '*', '/' };
+
+        static void Main(string[] args)
+        {
+            int i = 0;
+            bool foundOp = false;
+
+            string number1;
+            string number2;
+
+            float result = default;
+
+            foreach (char a in equation)
+            {
+                foreach (char b in operators)
+                {
+                    if (a == b)
+                    {
+                        foundOp = true;
+                        break;
+                    }
+
+                    else
+                    {
+                        foundOp = false;
+                        continue;
+                    }
+                }
+
+                if (foundOp == true && isFloat(equation.Substring(0, i)) && isFloat(equation.Substring(i + 1)))
+                {
+                    number1 = equation.Substring(0, i);
+                    number2 = equation.Substring(i + 1);
+
+                    if (equation[i] == '+')
+                    {
+                        result = float.Parse(number1) + float.Parse(number2);
+                    }
+
+                    else if (equation[i] == '-')
+                    {
+                        result = float.Parse(number1) - float.Parse(number2);
+                    }
+
+                    else if (equation[i] == '*')
+                    {
+                        result = float.Parse(number1) * float.Parse(number2);
+                    }
+
+                    else if (equation[i] == '/')
+                    {
+                        result = float.Parse(number1) / float.Parse(number2);
+                    }
+
+                    Console.WriteLine(result);
+                }
+
+                else
+                {
+                    i++;
+                }
+            }
+
+            bool isFloat(string s)
+            {
+                float myFloat;
+                return float.TryParse(s, out myFloat);
+            }
+        }
+    }
+}
